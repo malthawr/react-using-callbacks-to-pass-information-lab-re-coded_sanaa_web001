9e51182fa61b69bc39c6a3b3cc63d53088930ba1
Mohammed Al-Thawr <malthawr@gmail.com>
2020-09-22 16:24:43 +0000

    Automatically backed up by Learn

--- src/Cell.js
+++ src/Cell.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 
 export default class Cell extends Component {
-  
+
   constructor(props) {
     super()
     this.state = {
       color: props.color
     }
   }
-  
+
   handleClick = () => {
     this.setState({
       color: this.props.selectedColor
     })
   }
-  
+
   render() {
     return (
       <div className="cell"
@@ -23,5 +23,5 @@ export default class Cell extends Component {
       </div>
     )
   }
-  
+
 }
